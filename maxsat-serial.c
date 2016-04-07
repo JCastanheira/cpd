88d4c8262ec8b6a9c1998b1ae57256ce79536abe
rjri <rppguerra@gmail.com>
2016-04-07 17:28:38 +0100

    sem comentários

--- maxsat-serial.c
+++ maxsat-serial.c
@@ -14,17 +14,6 @@ int maxVar(int* linha){
 	}
 	return max;
 }
-/*
-int compMax(const void* linha1, const void* linha2){
-	if(maxVar((int*)linha1)>maxVar((int*)linha2)){
-		return 1;
-	}
-	if(maxVar((int*)linha1)<maxVar((int*)linha2)){
-		return -1;
-	}
-	return 0;
-}
-*/
 
 void searchTree(int node, int n_clauses, int mat[][20], int* status, int imp_clauses, int *maxsat, int *maxsat_count, int n_vars, int* best, int* current){
 	int i,j;
@@ -56,24 +45,16 @@ void searchTree(int node, int n_clauses, int mat[][20], int* status, int imp_cla
 			mscount++;
 		}
 	}
-	/*printf("node: %d\n",node);
-	for(i=0;i<n_clauses;i++){
-		printf("status: %d\n",status[i]);
-	}
-	printf("imp_clauses: %d\n",imp_clauses);
-	printf("mscount: %d\n",mscount);*/
 	if(mscount>(*maxsat)){
 		(*maxsat)=mscount;
 		for(i=0;i<n_vars;i++){
 			best[i]=current[i];
 		}
 		(*maxsat_count)=(int) pow(2.0,n_vars-abs(node));
-		/*printf("maxsat_count: %d %d\n",(*maxsat_count),2^(n_vars-abs(node)));*/
 		return;
 	}
 	if(mscount==(*maxsat)){
 		(*maxsat_count)+=(int) pow(2.0,n_vars-abs(node));
-		/*printf("maxsat_count: %d\n",(*maxsat_count));*/
 		return;
 	}
 	if(mscount<(*maxsat)&&mscount>=0){
@@ -92,7 +73,7 @@ int main(int argc, char ** argv){
 	char * fileNameIn;
 	char * fileNameOut;
 	FILE * fp;
-	char extOut[] = ".solng";/*mudar depois*/
+	char extOut[] = ".out";
 	char str[60];
 	int n_vars, n_clauses,n=0;
 	int i=0,j=0,num;
@@ -104,7 +85,6 @@ int main(int argc, char ** argv){
 	}
 	
 	fileNameIn= argv[1];
-	/*printf("%s\n", fileNameIn);*/
 	
 	fp  = fopen(fileNameIn, "r");
 	
@@ -114,37 +94,25 @@ int main(int argc, char ** argv){
 	}
 	fgets(str,60,fp);
 	sscanf(str,"%d %d", &n_vars, &n_clauses);
-	/*printf("%d %d\n", n_variables, n_clauses);*/
 	int mat[n_clauses][20];
 	for(i=0;i<n_clauses;i++){
 		for(j=0;j<20;j++){
 			mat[i][j]=0;
 		}
 	}
-	/*printf("Buff\n");*/
 	for(i = 0; i < n_clauses; i++){
 		fgets(str,60,fp);
-		/*printf("%s",str);*/
 		n=0,num=0;
 		while (sscanf(&(str[n]), "%d %n", &(mat[i][num]), &j) == 1) {
-			/*printf("%d\n",mat[i][num]);*/
 			n += j;
 			num++;			
-			/*sleep(1);*/
 		}
 	}
 	fclose(fp);
-	/*for(i=0;i<n_clauses;i++){
-		for(j=0;j<20;j++){
-			printf("%d ", mat[i][j]);
-		}
-		printf("\n");
-	}*/
 	int status[n_clauses];
 	for(i=0;i<n_clauses;i++){
 		status[i]=0;
 	}
-	/*printf("n_vars: %d\n",n_vars);*/
 	int maxsat=0, maxsat_count=0;
 	int best[n_vars],current[n_vars];
 	for(i=1;i<=n_vars;i++){
@@ -157,14 +125,6 @@ int main(int argc, char ** argv){
 	}
 	searchTree(1,n_clauses,mat,status,0,&maxsat,&maxsat_count,n_vars,&best[0],&current[0]);
 	printf("%d %d\n",maxsat,maxsat_count);
-	/*qsort(mat,n_clauses,20*sizeof(int),compMax);
-	printf("Sorted:\n");
-	for(i=0;i<n_clauses;i++){
-		for(j=0;j<20;j++){
-			printf("%d ", mat[i][j]);
-		}
-		printf("\n");
-	}*/
 	printf("Best:\n");
 	for(i=0;i<n_vars;i++){
 		printf("%d ",best[i]);
